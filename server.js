e847ab33f8570719314368805d045e74fa91c17a
Bruno Sagnardon <bruno.sagnardon@gmail.com>
2026-03-08 16:30:15 +0100

    Increase request body size limit for large email imports

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ const loginAttempts = new Map()
 const sessionMaxAgeMs = 1000 * 60 * 60 * 24 * 7
 
 app.use(helmet())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json({ limit: "2mb" }))
+app.use(express.urlencoded({ extended: true, limit: "2mb" }))
 
 app.use(session({
  name:"discord_access_admin",
